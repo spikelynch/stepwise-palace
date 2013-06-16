ca226fecefe217f20bd3e304887533d4090c7a6e
Mike Lynch <mike.lynch@iinet.net.au>
2013-06-16 23:18:34 +1000

    Stanzas are appearing with linebreaks!

--- Reader/stepwise_rotate.js
+++ Reader/stepwise_rotate.js
@@ -4,20 +4,23 @@
 var rot90 = [ [ 0, -1 ], [ 1, 0 ] ];
 var rot270 = [ [ 0, 1 ], [ -1, 0 ] ];
 
-var WIDTH = 800;
+var WIDTH = 1200;
 var HEIGHT = 600;
 
 var CX = WIDTH / 2;
 var CY = HEIGHT / 2;
 
-var SIZE = 80;
-var XSPACE = 90;
+var SIZE = 120;
+var XSPACE = 140;
 var WXSPACE = 3 * XSPACE + 20;
 var WYSPACE = XSPACE;
 var WZSPACE = 100;
 var SCREEN = 400;
 var ZSPACE = XSPACE;
 var OFFSET = SIZE / 2;
+
+var LEADING = SIZE / 10;
+var TEXT_OFFSET = LEADING + 1;
 
 var ROTATE_TIME = 2000;
 
@@ -52,7 +55,7 @@ function wfact(w) {
 
 function room_opacity(room) {
     if( room.coords[2] == 1 && room.coords[3] == 0 ) {
-        return 0.6;
+        return .75;
     } else {
         return 0.2;
     }
@@ -64,23 +67,39 @@ function hypercube(elt) {
         .attr("width", WIDTH)
 	    .attr("height", HEIGHT);
 
-    nodes = svg.selectAll("rect")
+    nodes = svg.selectAll("g")
     	.data(rooms)
     	.enter()
-    	.append("rect")
-        .attr("class", function(d) { return "cell" + d.coords[2] } )
-        .attr("width", SIZE)
-        .attr("height", SIZE)
-    	.attr("transform", function(d) { 
+    	.append("g")
+        .attr("transform", function(d) { 
             var t = room_position(d.coords);
             console.log(t);
             return t;
         } )
         .attr("opacity", room_opacity);
 
-
+    nodes.append("rect")
+        .attr("class", function(d) { return "cell" + d.coords[2] } )
+        .attr("width", SIZE)
+        .attr("height", SIZE);
+   
+    nodes.each(add_stanza);
 }
 
+
+function add_stanza(d, i) {
+
+    var lines = stanzas[d.name];
+
+    for( l = 0; l < lines.length; l++ ) {
+        d3.select(this).append("text")
+	        .attr("text-anchor", "start")
+	        .attr("class", "poem")
+	        .attr("dx", 0)
+	        .attr("dy", TEXT_OFFSET + l * LEADING)
+            .text(lines[l]);
+    }
+}
 
 
 function rotate_render(c1, c2) {
